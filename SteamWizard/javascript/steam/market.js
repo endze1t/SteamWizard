0626942984cdf7e6005c4ef6b1d197af6a25f3b6
metjm <metjm.mail@gmail.com>
2016-06-24 23:45:14 +0200

    moved everything in screenshots.js to metjm.js, adjusted updated interval

--- SteamWizard/javascript/steam/market.js
+++ SteamWizard/javascript/steam/market.js
@@ -1,11 +1,5 @@
 "using strict";
 
-var STEAM_WIZARD_CONFIG = {
-    pagingInterval: null,
-    enabled: true,
-    token: null,
-};
-
 function createSteamButton(text) {
     var $output = $("<div></div>");
     $output.addClass('btn_green_white_innerfade btn_small steam_wizard_load_button');
@@ -28,15 +22,25 @@ function onGetFloat() {
     $getFloatButton.off();
     $getFloatButton.text('loading...').addClass('btn_grey_white_innerfade');
 
-    csgozone.market(inspectLink, function(data) {
-        if(data.success === true) {
-           $getFloatButton.text(data.wear.toFixed(15));
-           $getFloatButton.removeClass('btn_grey_white_innerfade').addClass('btn_blue_white_innerfade');
-        } else {
-           $getFloatButton.text('Failed').addClass('steam_wizard_load_button_failed');
-           $getFloatButton.click(onGetFloat);
-        }
-    });
+    $.ajax({type: "POST", 
+            url: "https://www.csgozone.net/_service/plugin", 
+            data: "type=marketInspect&link="+ encodeURIComponent(inspectLink),
+            xhrFields: {withCredentials: true}})
+        .done(function(data) {
+            if(data.success === false) {
+               $getFloatButton.text('Failed').addClass('steam_wizard_load_button_failed');
+               $getFloatButton.on(onGetFloat);
+               return;
+            }
+            
+            $getFloatButton.text(data.wear.toFixed(15));
+            $getFloatButton.removeClass('btn_grey_white_innerfade').addClass('btn_blue_white_innerfade');
+        }).fail(function(jqXHR, textStatus, errorThrown) { 
+            $getFloatButton.text('Failed').addClass('steam_wizard_load_button_failed');
+            $getFloatButton.on(onGetFloat);
+        }).always(function() {         
+            
+        });
 }
 
 function onGetAllFloats() {
@@ -56,15 +60,16 @@ function showScreenshotPopup(image_url) {
 function onGetScreenshot() {
 	var $marketListingRow = $(this.closest('.market_listing_row'));
 	var inspectLink = getInspectLink($marketListingRow);
+	var token = '';
 	
 	var $getScreenshotButton = $marketListingRow.find(".steam_wizard_load_button_screenshot").first();
 	$getScreenshotButton.off();
 	$getScreenshotButton.text('loading...').addClass('btn_grey_white_innerfade');
-	Screenshots.requestScreenshot(inspectLink, function(result){
+	metjm.requestScreenshot(inspectLink, token, function(result){
 		if (result.success) {
-			if(result.result.status == Screenshots.STATUS_QUEUE){
+			if(result.result.status == metjm.STATUS_QUEUE){
 				$getScreenshotButton.text('Queue: ' + result.result.place_in_queue).addClass('btn_grey_white_innerfade');
-			}else if (result.result.status == Screenshots.STATUS_DONE){
+			}else if (result.result.status == metjm.STATUS_DONE){
 				$getScreenshotButton.text('Open Screenshot');
 				$getScreenshotButton.removeClass('btn_grey_white_innerfade').addClass('btn_blue_white_innerfade');
 				$getScreenshotButton.click(function(){
@@ -80,20 +85,7 @@ function onGetScreenshot() {
 	});
 }
 
-function handlePaging() {
-    var searchResults_start = $('#searchResults_start').text();
-    var counter = 0;
-    clearInterval(STEAM_WIZARD_CONFIG.pagingInterval);
-    STEAM_WIZARD_CONFIG.pagingInterval = setInterval(function() {
-        /* limit to 5 trials */
-        if($('#searchResults_start').text() === searchResults_start && counter++ < 5)
-           return;
-
-        initButtons();
-        clearInterval(STEAM_WIZARD_CONFIG.pagingInterval);
-    }, 1000);
-}
-
+var steamWizardButtonInterval;
 function initButtons() {
     $("#searchResultsRows").find(".market_listing_row").each(function(index, marketListingRow) {
         var $marketListingRow = $(marketListingRow);
@@ -119,45 +111,31 @@ function initButtons() {
         $getAllFloatsButton.on('remove', function() {alert('removed');});
 
         var $container = $(".market_listing_header_namespacer").parent();
+        //$container.empty();
         $container.append($getAllFloatsButton);
     }
     
     /* other pages too */
-    $('#searchResults_links').find('.market_paging_pagelink').on('click', handlePaging);
-}
-
-function removeButtons() {
-    $("#searchResultsRows").find('.steam_wizard_load_button_float').remove();
-    $("#searchResultsRows").find('.steam_wizard_load_button_screenshot').remove();
-    $("#searchResultsRows").find('.steam_wizard_load_button_float_all').remove();
-    
-    $('#searchResults_links').find('.market_paging_pagelink').off('click', handlePaging);
-}
-
-function init() {
-    console.log("init");
-    /* make sure both services are enabled */
-    if(STEAM_WIZARD_CONFIG.token !== null) {
-       csgozone.setToken(STEAM_WIZARD_CONFIG.token);
-       metjm.setToken(STEAM_WIZARD_CONFIG.token);
-    }
-    
-    var port = chrome.runtime.connect();
-    
-    port.onMessage.addListener(function(request, port) {
-        switch(request.msg) {
-            case 'pluginStatus':
-                STEAM_WIZARD_CONFIG.enabled = request.status;
-                
-            if(STEAM_WIZARD_CONFIG.enabled)
-               initButtons();
-            else
-               removeButtons();
-        }
+    $('#searchResults_links').find('.market_paging_pagelink').on('click', function() {
+        var searchResults_start = $('#searchResults_start').text();
+        var counter = 0;
+        clearInterval(steamWizardButtonInterval);
+        steamWizardButtonInterval = setInterval(function() {
+            /* limit to 5 trials */
+            if($('#searchResults_start').text() === searchResults_start && counter++ < 5)
+               return;
+            
+            initButtons();
+            clearInterval(steamWizardButtonInterval);
+        }, 1000);
     });
-    
-    port.postMessage({msg: 'getPluginStatus'});
-    
+}
+
+function init(){
+    console.log('init()');
+
+    initButtons();
+
     /* build sceenshot overlay */
     var $overlay = $('<div>');
     $('<img>').appendTo($overlay);
@@ -177,41 +155,17 @@ function init() {
     });
 }
 
-function validateToken(token) {
-    if(token == null)
-       return false;
-   
-    try {
-        var json = JSON.parse(atob(token));
-    } catch(e) {
-        return false;
-    }
-    
-    if(json.timestamp == null || new Date().getTime() - json.timestamp > 2 * 24 * 60 * 60 * 1000)
-       return false;
-    
-    return true;
+function getTurnedOff(callback){
+	chrome.runtime.sendMessage({msg: "getTurnedOff"}, function(response) {
+		callback(response.turnedOff);
+	});
 }
 
 $(document).ready(function() {
-    var token = window.localStorage.getItem('steam_wizard_token');
-    
-    if(validateToken(token))
-       STEAM_WIZARD_CONFIG.token = token;
-    else
-       window.localStorage.removeItem('steam_wizard_token')
-
-    function loginCallback(response) {
-        console.log("login callback");
-        if(response.success === true) {
-           STEAM_WIZARD_CONFIG.token = response.token;
-           window.localStorage.setItem('steam_wizard_token', response.token);
-        }
-    }
-    
-    /* TODO: LOADING INDICATION */
-    if(STEAM_WIZARD_CONFIG.token === null) {
-       $.when(csgozone.login(loginCallback), metjm.login(loginCallback)).then(init);
-    } else
-        init();
+	
+	getTurnedOff(function(turnedOff){
+		console.log('turnedOff: ' + turnedOff);
+	});
+	
+    init();
 });
--- SteamWizard/javascript/utils/metjm.js
+++ SteamWizard/javascript/utils/metjm.js
@@ -1,8 +1,39 @@
+/*http://stackoverflow.com/a/18405800*/
+if (!String.prototype.format) {
+	String.prototype.format = function() {
+		var args = arguments;
+		return this.replace(/{(\d+)}/g, function(match, number) {
+			return typeof args[number] != 'undefined'
+				? args[number]
+				: match
+			;
+		});
+	};
+}
+
+function metjm_loadLocalStorage(){
+	try{
+		var output = JSON.parse(window.localStorage.getItem('steam_wizard_inspect_cache_object'));
+		if (output)
+			return output;
+	}catch(e){
+	}
+	return {
+		orderList : [],
+		hashMap : {}
+	};
+}
+
 var metjm = {
-	
 	API_URL : "https://metjm.net/shared/extension.php",
+	API_REQUEST_NEW : "https://metjm.net/shared/screenshots-v5.php?cmd=request_new_link&inspect_link={0}&user_uuid={1}&user_client=3&token={2}",
+	API_REQUEST_STATUS : "https://metjm.net/shared/screenshots-v5.php?cmd=request_screenshot_status&id={0}",
 	LOGIN_REQUEST : "cmd=get_token",
     token: '',
+	STATUS_QUEUE : 1,
+	STATUS_DONE : 2,
+	STATUS_FAIL : 3,
+	inspectCache : metjm_loadLocalStorage(''),
     
     login: function(callback) {
         return $.ajax({type: "POST", 
@@ -21,5 +52,66 @@ var metjm = {
     
     setToken: function(token) {
         this.token = token;
-    }
+    },
+	
+	saveInspectLink : function(inspectLink, resultObject){
+		if (!this.inspectCache.hashMap[inspectLink])
+			this.inspectCache.orderList.push(inspectLink);
+		this.inspectCache.hashMap[inspectLink] = resultObject;
+		
+		if (this.inspectCache.orderList.length > 20){
+			delete this.inspectCache.hashMap[this.inspectCache.orderList[0]];
+			this.inspectCache.orderList.splice(0,1);
+		}
+		
+		window.localStorage.setItem('steam_wizard_inspect_cache_object', JSON.stringify(this.inspectCache));
+		console.log(this.inspectCache);
+	},
+	
+	getCachedLink : function(inspectLink){
+		return this.inspectCache.hashMap[inspectLink];
+	},
+	
+	requestScreenshot : function(inspectLink, token, callback){
+		var cached = metjm.getCachedLink(inspectLink);
+		if (cached){
+			if (cached.success && cached.result.status == metjm.STATUS_QUEUE){
+				metjm.updateScreenshot(cached.result.screen_id, callback, inspectLink);
+				return;
+			}else{
+				callback(cached);
+				return;
+			}
+		}
+		
+		var requestUrl = metjm.API_REQUEST_NEW.format(inspectLink, "", token);
+		$.getJSON(requestUrl, function(result) {
+			if (result.success){
+				metjm.updateScreenshot(result.result.screen_id, callback, inspectLink);
+			}else{
+				callback({success:false});
+			}
+		});
+	},
+	
+	updateScreenshot : function(screen_id, callback, inspectLink){
+		var requestUrl = metjm.API_REQUEST_STATUS.format(screen_id);
+		$.getJSON(requestUrl, function(result) {
+			callback(result);
+			
+			var updateInterval = 15000;
+			if (result.result.prem == 1)
+				updateInterval = 5000;
+			
+			//keep updating if still in queue
+			if(result.success == true && result.result.status == metjm.STATUS_QUEUE){
+				metjm.saveInspectLink(inspectLink, result);
+				setTimeout(function(){
+					metjm.updateScreenshot(screen_id, callback, inspectLink);
+				},updateInterval);
+			}else if (result.success == true && result.result.status == metjm.STATUS_DONE){
+				metjm.saveInspectLink(inspectLink, result);
+			}
+		});
+	}
 }
